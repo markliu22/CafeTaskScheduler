c1e91d17cfe0f62cd332c3d99112c745590eac93
Mark Liu <markliu202@gmail.com>
2021-09-07 22:18:09 -0400

    cleanup

--- cafe.cpp
+++ cafe.cpp
@@ -1,57 +1,26 @@
-/*
-INSPIRED BY THIS TALK: https://www.youtube.com/watch?v=jJS6G7irZSc&t=818s&ab_channel=CodingTech
-5 latte orders are placed at exactly the same time.
-To make a latte, need to:
-1) grind beans
-2) make expresso
-3) steam milk
-4) assemble
-1) requires a machine (grinder)
-2) requires a machine (press)
-3) requires a machine (steamer)
-4) requries nothing
-global _finished = 0
-For each order, create a latte struct which has its own thread.
-  In the function that we pass to this thread:
-    while not done all of steps 1), 2), 3)
-      try doing 1)
-      try doing 2)
-      try doing 3)
-    do 4)
-*/
+// https://www.youtube.com/watch?v=jJS6G7irZSc&t=818s&ab_channel=CodingTech
 
-
-// I/O
-#include <iostream>
-// multithreading 
 #include <mutex>
 #include <thread>
 #include <atomic>
 #include <chrono>
-//
+
+#include <iostream>
 #include <array>
 #include <string>
 #include <random>
 
-// mutex for printing
+// mutex just for printing
 std::mutex g_lockprint;
-
-// global atomic int for number of orders complete
-// std::atomic<int> num_complete( 0 );
-
-// global const for number of orders to start
 const int num_orders = 5; 
 
-// TODO: eventually move the print function to here and also add the logic of marking the step complete to here
 struct Grinder {
   std::mutex mutex; 
 };
-
 // TODO: make this multiple parts so we actually need multiple mutexes at once to operate this press
 struct Press {
   std::mutex mutex;
 };
-
 struct Steamer {
   std::mutex mutex;
 };
@@ -89,21 +58,21 @@ struct Latte {
   }
 
   void use_grinder() {
-    // If grinder mutex already locked, skip this (aka move onto the use_press() step so we can try again later)
+    // If grinder mutex already locked, skip this (move onto the use_press() step so we can try again later)
     if(grinder_ref.mutex.try_lock()) {
       // The try_lock above was successful, this thread already has ownership of the grinder_ref.mutex
       // **still** want to use lock_guard
       // This is the use case for adopt_lock => "assume the calling thread already has ownership of the mutex"
       std::lock_guard<std::mutex> grinder_lock(grinder_ref.mutex, std::adopt_lock);
       print(" is using grinder");
-      // other threads continue their execution, this just blocks current thread for the time specified
+      // other threads continue their execution, this only blocks current thread for the time specified
       std::random_device rd;
       std::mt19937 gen(rd());
       std::uniform_int_distribution<> distr(1, 6);
       std::this_thread::sleep_for (std::chrono::seconds(distr(gen)));  
       grind_complete = true;
     }
-    // mutex would be unlocked here
+    // mutex would be unlocked here if manually locked mutex instead of using lock_guard
   }
 
   void use_press() {
@@ -142,7 +111,7 @@ struct Latte {
 // creates Grinder, Press, Steamer
 // creates 5 Latte orders
 void shop_start() {
-  std::cout << "Starting shop" << std::endl;
+  std::cout << "starting shop" << std::endl;
   Grinder g; // Grinder/Press/Steamer all have default constructors and calling them here
   Press p;
   Steamer s;
